927f97efab4f1fbf59ca2450fb30a1b0651b9174
DennisDv24 <DennisDv24@proton.me>
2023-05-07 06:23:02 +0200

    ScatterAuction smoke test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {
+            mining: {
+                auto: true,
+                interval: 1000
+            }
+        }
+    }
 };
 
 export default config;
